435a47b45af0d7ac128ff621112673637026d6ad
David Laribee <david@laribee.com>
2008-02-01 00:05:26 +0000

    implemented the ListCommand with specs. need to go to ILocalRepository impl next and this chick is toast.

--- src/nu/Commands/ListCommand.cs
+++ src/nu/Commands/ListCommand.cs
@@ -7,16 +7,29 @@ namespace nu.Commands
     [Command(Description = "List packages available in the local repository.")]
     public class ListCommand : ICommand
     {
-       private ILocalPackageRepository _localPackageRepository;
+       private readonly ILocalPackageRepository _localPackageRepository;
+       private readonly IConsoleHelper _consoleHelper;
 
-       public ListCommand(ILocalPackageRepository localPackageRepository)
+       public ListCommand(ILocalPackageRepository localPackageRepository, IConsoleHelper consoleHelper)
        {
           _localPackageRepository = localPackageRepository;
+          _consoleHelper = consoleHelper;
        }
 
        public void Execute(IEnumerator<IArgument> arguments)
        {
-          
+          IEnumerable<Package> packages = _localPackageRepository.FindAll();
+
+          if (packages == null)
+          {
+             _consoleHelper.WriteLine("No packages installed. Get to it!");
+             return;
+          }
+
+          foreach(Package package in packages)
+          {
+             _consoleHelper.WriteLine(package.Name);
+          }
        }
     }
 }
