54cc676c7ab742a7d346bc0061d5e6c53f7e945e
Dru Sellers <dru@drusellers.com>
2008-09-15 20:56:02 +0000

    adding assemblyinfo for the C# projects

---
+++ src/nu/config/project/cs-20/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("${ProjectName}")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("${ProjectName}")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("2a1bdcf4-ef63-41e1-93d0-1a441117f59a")]
