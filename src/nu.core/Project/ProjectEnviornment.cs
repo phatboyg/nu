b33b026e242f8752585327596dec58cf66983a4e
drusellers <dru@drusellers.com>
2010-02-09 21:11:40 -0600

    removing old IConsole idea

--- src/nu.core/Project/ProjectEnviornment.cs
+++ src/nu.core/Project/ProjectEnviornment.cs
@@ -1,19 +1,28 @@
-using System;
-using System.IO;
-using nu.Utility;
-
-namespace nu.Model.Project
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace nu.core.Project
 {
+    using nu.Model.Project;
+
     public class ProjectEnvironment : IProjectEnvironment
     {
-        private readonly string _projectDirectory;
-        private readonly string _projectName;
-        private readonly bool _isTemplate;
+        readonly bool _isTemplate;
+        readonly string _projectDirectory;
+        readonly string _projectName;
 
         public ProjectEnvironment(string projectName, string projectDirectory)
             : this(projectName, projectDirectory, false)
         {
-
         }
 
         public ProjectEnvironment(string projectName, string projectDirectory, bool isTemplate)
