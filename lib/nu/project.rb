01df3486382daf730b18b36b23c3770c4ba38d07
drusellers <dru@drusellers.com>
2010-07-17 14:29:57 -0500

    whoops

--- lib/nu/project.rb
+++ lib/nu/project.rb
@@ -1,29 +1,30 @@
-class Project
+module Nu
+  class Project
   
-  
-  def initialize
-    @config_file = ".nu/options.yaml"
-  end
+    def initialize
+      @config_file = ".nu/options.yaml"
+    end
 
-  def ensure_default_config
-    if File.exist? @config_file
-			return
-		end
-		add_file @config_file
-		content = YAML::dump( {'lib'=>'lib'} )
-		append_file @config_file, content 
-  end
+    def ensure_default_config
+      if File.exist? @config_file
+  			return
+  		end
+  		add_file @config_file
+  		content = YAML::dump( {'lib'=>'lib'} )
+  		append_file @config_file, content 
+    end
   
-  def get_location
-		content = YAML.load_file @config_file
-		content['lib']
-	end
+    def get_location
+  		content = YAML.load_file @config_file
+  		content['lib']
+  	end
 	
-	def set_location(name)
-	  File.delete @config_file if File.exist? @config_file
+  	def set_location(name)
+  	  File.delete @config_file if File.exist? @config_file
 
-		content = YAML::dump( { 'lib' => name })
+  		content = YAML::dump( { 'lib' => name })
 		
-		File.open(@config_file, 'w') {|f| f.write(content) }
+  		File.open(@config_file, 'w') {|f| f.write(content) }
+    end
   end
 end
